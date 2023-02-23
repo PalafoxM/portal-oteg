bb6114e41ff999872acbd5c38b7fd72751396697
R0g3r-SH <Rogelio84@hotmail.com>
2023-02-23 11:33:48 -0600

    Dynamic PDFS

--- config/db.py
+++ config/db.py
@@ -4,31 +4,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # mysqlclient
 
-# MYSQL = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'otegDEV',
-#         'USER': 'oteg-dev',
-#         'PASSWORD': 'OTEG2023*Purpura.',
-#         'HOST': 'sectur-gto.c31dkeyomhn9.us-east-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'sql_mode': 'traditional',
-#         }
-#     }
-# }
-
-
 MYSQL = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'NAME': 'otegDEV',
+        'USER': 'oteg-dev',
+        'PASSWORD': 'OTEG2023*Purpura.',
+        'HOST': 'sectur-gto.c31dkeyomhn9.us-east-2.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
         }
     }
 }
+
