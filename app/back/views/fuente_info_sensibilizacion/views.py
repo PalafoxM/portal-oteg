d96fc68fd050d990d39794a187afea4ef55ebc7e
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-05-29 16:55:49 -0600

    sencibilidad

--- app/back/views/fuente_info_sensibilizacion/views.py
+++ app/back/views/fuente_info_sensibilizacion/views.py
@@ -6,16 +6,21 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from back.models import *
 from back.forms import *
 from web.models import *
-from django.shortcuts import render
 from django.http import HttpResponse
-import csv
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.views.decorators.http import require_GET
-from django.views.decorators.http import require_POST
 from django.template.loader import render_to_string
+# para archivo excel
+from django.views import View
+from django.contrib import messages
+from openpyxl import load_workbook
+import csv
+import os
+from datetime import datetime
+from django.urls import reverse
+import openpyxl
+from django.http import HttpResponse
+import json
+from config.diccionarios import clean_str_col, homologar_columna_categoria, homologar_columna_destino
 
 
 def is_ajax(request):
@@ -32,6 +37,7 @@ class FuenteInfoSensibilizacion (ListView):
         context['create_url'] = reverse_lazy('dashboard:fuente_info_sensibilizacion_create')
         context['entity'] = 'Sesibilizacion'
         context['is_fuente'] = True
+        context['carga_masiva_url'] = reverse_lazy('dashboard:fuente_info_sensibilizacion_carga_masiva')
         return context  
     
 class FuenteInfoSensibilizacionCreate (CreateView):
@@ -201,12 +207,262 @@ class FuenteInfoSensibilizacionUpdate (UpdateView):
         context['edit_msg'] = 'Los Campos Destino y Fecha no pueden ser editados' 
 
         return context
-
-
-    
+   
 class FuenteInfoSensibilizacionDelete (DeleteView):
     model = Sesibilizacion
     success_url = reverse_lazy('dashboard:fuente_info_sensibilizacion') 
 
     def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
         return super().post(request, *args, **kwargs)
+
+class SesibilizacionCargaMasivaView(View):
+    form_class = CargaMasivaForm
+    template_name = 'back/fuente_info_sensibilizacion/carga_masiva.html'
+    success_url = reverse_lazy('dashboard:fuente_info_sensibilizacion')
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form, 'title': 'Carga Masiva de Sesibilizacion'})
+
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        registros_correctos, registros_incorrectos, registros_existentes = [], [], []
+        num_filas_procesadas = 0
+        archivo = request.FILES.get('archivo', None)
+        if archivo:
+            extension = os.path.splitext(archivo.name)[1]
+            if extension == '.xlsx':
+                registros_correctos, registros_incorrectos, registros_existentes, num_filas_procesadas = self.procesar_archivo_xlsx(archivo)
+            elif extension == '.csv':
+                registros_correctos, registros_incorrectos, registros_existentes, num_filas_procesadas = self.procesar_archivo_csv(archivo)
+            else:
+                messages.error(request, 'El archivo debe ser un archivo .xlsx o .csv')
+                registros_incorrectos.append("El archivo debe ser un archivo .xlsx o .csv")
+        else:
+            messages.error(request, 'Debe seleccionar un archivo')
+            registros_incorrectos.append("Debe seleccionar un archivo")
+
+        if len(registros_incorrectos) > 0 or len(registros_existentes) > 0:
+            messages.error(request, 'Hay errores de registros')
+            datos_json = json.dumps(registros_incorrectos)
+            
+            return render(request, self.template_name, {
+                'form': form,
+                'title': 'Carga Masiva de Sesibilizacion',
+                'registros_correctos': registros_correctos,
+                'registros_incorrectos': registros_incorrectos,
+                'registros_existentes': registros_existentes,
+                'descargar_url': datos_json,
+                'num_filas_procesadas': num_filas_procesadas,
+            })
+            
+        else:
+            return HttpResponseRedirect(reverse('dashboard:inversion_publica_list'))
+        
+        
+
+    def procesar_archivo_xlsx(self, archivo):
+        registros_correctos, registros_incorrectos, registros_existentes = [], [], []
+        num_filas_procesadas = 0
+        try:
+            workbook = load_workbook(filename=archivo, read_only=True)
+            worksheet = workbook.active
+            filas = list(worksheet.rows)
+            for i, row in enumerate(filas):
+                if i == 0:
+                    continue # Ignorar la primera fila si es el encabezado
+                num_filas_procesadas += 1
+                # Limpieza de datos
+                fecha_str = row[0].value if len(row) > 0 and isinstance(row[0].value, str) else ''
+                
+
+                participantes = row[3].value
+                accion_de_sensibilizacion = row[2].value
+                subcategoria = row[4].value
+
+                # Limpieza de datos
+                destino = clean_str_col(row[1].value)
+
+                # Homologación de datos
+                destino = homologar_columna_destino(destino)
+
+                datos = {
+                    "destino": destino,
+                    "fecha": fecha_str,
+                    "participantes": participantes,
+                    "accion_de_sensibilizacion": accion_de_sensibilizacion,
+                    "subcategoria": subcategoria
+                }
+
+                try:
+                    # Validar los datos
+                    participantes_int = int(participantes)
+
+                    if destino not in CatalagoDestino.objects.values_list('destino', flat=True):
+                        print(f"El destino {destino} no está en la tabla CatalagoDestino")
+                        registros_incorrectos.append(datos)
+                        continue
+
+                    # Buscar si la fila ya existe en la base de datos
+                    existente = Sesibilizacion.objects.filter(
+                        destino = destino, 
+                        participantes = participantes_int,
+                        fecha = fecha_str, 
+                        accion_de_sensibilizacion = accion_de_sensibilizacion)
+                    if existente.exists():
+                        # Si ya existe, se omite la fila y se guarda en la lista de registros incorrectos
+                        print(f"La fila {datos} ya existe en la base de datos")
+                        registros_existentes.append(datos)
+                    else:
+                        # Si no existe, se guarda la nueva instancia del modelo en la base de datos y se guarda en la lista de registros correctos
+                        db = Sesibilizacion(
+                            destino = destino, 
+                            participantes = participantes_int,
+                            fecha = fecha_str, 
+                            accion_de_sensibilizacion = accion_de_sensibilizacion,
+                            subcategoria = subcategoria
+                        )
+                        db.save()
+                        registros_correctos.append(datos)
+                except (ValueError, TypeError) as e:
+                    # Si los datos no son válidos, se guarda el número de fila en la lista de registros incorrectos
+                    registros_incorrectos.append(datos)
+                    
+        except FileNotFoundError:
+                print(f"El archivo {archivo} no se pudo abrir")
+                
+        return registros_correctos, registros_incorrectos, registros_existentes, num_filas_procesadas
+    
+    def procesar_archivo_csv(self, archivo):
+        archivo = self.request.FILES['archivo']
+        registros_correctos, registros_incorrectos, registros_existentes = [], [], []
+        num_filas_procesadas = 0
+        try:
+            datos = csv.DictReader(archivo.read().decode('latin-1').splitlines())
+            # print(datos)
+            for row in datos:
+                num_filas_procesadas += 1
+
+                fecha = row['fecha']
+                fecha_str = str(fecha)
+                fecha_str = fecha_str.split()[0] if fecha_str else ''  # Eliminar la parte de la hora si existe la fecha
+                fecha_obj = datetime.datetime.strptime(fecha_str, '%Y-%m-%d').date()
+                # Serializar la fecha en formato JSON
+                json_fecha = json.dumps(fecha_obj.strftime('%Y-%m-%d'))
+                json_fecha = str(json_fecha).strip('"')
+
+                participantes = row['participantes']
+                accion_de_sensibilizacion = row['accion_de_sensibilizacion']
+                subcategoria = row['subcategoria']
+
+                # Limpieza de datos
+                destino = clean_str_col(row['destino'])
+
+                # Homologación de datos
+                destino = homologar_columna_destino(destino)
+
+                datos = {
+                    "destino": destino,
+                    "fecha": json_fecha,
+                    "participantes": participantes,
+                    "accion_de_sensibilizacion": accion_de_sensibilizacion,
+                    "subcategoria": subcategoria
+                }
+
+                try:
+                    # Validar los datos
+                    participantes_int = int(participantes)
+
+                    if destino not in CatalagoDestino.objects.values_list('destino', flat=True):
+                        print(f"El destino {destino} no está en la tabla CatalagoDestino")
+                        registros_incorrectos.append(row)
+                        continue
+
+                    # Buscar si la fila ya existe en la base de datos
+                    existente = zonas_arqueologicas_museos.objects.filter(
+                        destino = destino, 
+                        participantes = participantes_int,
+                        fecha = fecha_obj, 
+                        accion_de_sensibilizacion = accion_de_sensibilizacion)
+                    if existente.exists():
+                        # Si ya existe, se omite la fila y se guarda en la lista de registros incorrectos
+                        print(f"La fila {row} ya existe en la base de datos")
+                        registros_existentes.append(row)
+                    else:
+                        # Si no existe, se guarda la nueva instancia del modelo en la base de datos y se guarda en la lista de registros correctos
+                        db = zonas_arqueologicas_museos(
+                            destino = destino, 
+                            participantes = participantes_int,
+                            fecha = fecha_obj, 
+                            accion_de_sensibilizacion = accion_de_sensibilizacion,
+                            subcategoria = subcategoria
+                        )
+                        db.save()
+                        registros_correctos.append(row)
+                except (ValueError, TypeError) as e:
+                    print(f"Error al procesar la fila {row}: {e}")
+                    registros_incorrectos.append(row)
+        except FileNotFoundError:
+            print(f"No se encontró el archivo {archivo}")
+        except Exception as e:
+            print(f"Error al procesar el archivo {archivo}: {e}")
+        return registros_correctos, registros_incorrectos, registros_existentes, num_filas_procesadas
+
+class SesibilizacionDescargarArchivoView(View):
+
+    def crear_archivo_excel(self, registros_incorrectos):
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+
+        # Escribir encabezados de columna
+        worksheet['A1'] = 'fecha'
+        worksheet['B1'] = 'destino'
+        worksheet['C1'] = 'accion_de_sensibilizacion'
+        worksheet['D1'] = 'participantes'
+        worksheet['E1'] = 'subcategoria'
+
+        # Add the incorrect rows to the worksheet
+        for i, row in enumerate(registros_incorrectos):
+            fila = i + 2
+            worksheet.cell(row=fila, column=1, value=row['fecha'])
+            worksheet.cell(row=fila, column=2, value=row['destino'])
+            worksheet.cell(row=fila, column=3, value=row['accion_de_sensibilizacion'])
+            worksheet.cell(row=fila, column=4, value=row['participantes'])
+            worksheet.cell(row=fila, column=5, value=row['subcategoria'])
+
+
+
+        # Set the column widths to auto-fit
+        for column in worksheet.columns:
+            max_length = 0
+            column_name = column[0].column_letter
+            for cell in column:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(str(cell.value))
+                except:
+                    pass
+            adjusted_width = (max_length + 2)
+            worksheet.column_dimensions[column_name].width = adjusted_width
+
+        # Create the response with the Excel file
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=sensibilizacion_registros_incorrectos.xls'
+
+        
+
+        # workbook.save(response)
+        return workbook
+    
+    def post(self, request, *args, **kwargs):
+        # Obtener los registros incorrectos del cuerpo de la petición
+        registros_incorrectos = json.loads(request.body)
+
+        # Crear y enviar el archivo de Excel con las filas incorrectas
+        workbook = self.crear_archivo_excel(registros_incorrectos)
+
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=sensibilizacion_registros_incorrectos.xlsx'
+        workbook.save(response)
+        return response   
