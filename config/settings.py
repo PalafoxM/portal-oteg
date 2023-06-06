40f26a8e9c5269b2eb7c3333a49795d680cd80e4
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-06-06 11:11:49 -0600

    csrf

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'config.context_processors.global_variables'
+                'config.context_processors.global_variables',
+                'django.template.context_processors.csrf'
 
             ],
         },
