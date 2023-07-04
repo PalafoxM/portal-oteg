ef6df58256f7f0b7800849106d2347439684c74e
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-07-03 22:23:25 -0600

    diseños

--- app/web/views/sustentabilidad/views.py
+++ app/web/views/sustentabilidad/views.py
@@ -4,6 +4,6 @@ class Sustentabilidad(TemplateView):
     template_name = 'web/paginas/sustentabilidad/sustentabilidad.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['nav_title'] = 'Sustentabilidad'
-        context['img_url'] = 'img_nav/Portada-7.jpg'
+        context['nav_title'] = 'SUSTENTABILIDAD'
+        context['img_url'] = 'img_nav/SUSTENTABILIDAD.jpg'
         return context
