52d6f45a4fec35410f936fec024ee5f822d252ed
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-07-13 13:26:48 -0600

    correcciones finales

--- app/web/views/entornonacional/views.py
+++ app/web/views/entornonacional/views.py
@@ -6,7 +6,7 @@ class EntornoNacional(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['nav_title'] = 'ENTORNO NACIONAL'
-        context['img_url'] = 'img_nav/foros.jpg'
+        context['img_url'] = 'img_nav/Foros.jpg'
         return context
 class EntornoNacionalIndicadores(TemplateView):
     template_name = 'web/paginas/entorno-nacional/indicadores.html'
