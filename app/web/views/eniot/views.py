a3cf0e2ddd171b35739cd01449ebbe3d34d5b63d
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-07-17 16:39:39 -0600

    cambio en las portadas

--- app/web/views/eniot/views.py
+++ app/web/views/eniot/views.py
@@ -22,7 +22,7 @@ class EniotView(TemplateView):
         pdf =  Eniot.objects.filter(seccion='programa-eniot').order_by('-date_created')[:1].get()
         context['pdf'] = pdf
         context['nav_title'] = 'ENIOT'
-        context['img_url'] = 'img_nav/phone.jpg'
+        context['img_url'] = 'img_nav/evento-mesa.jpg'
         context['encuesta'] = self.encuesta 
         return context
     
