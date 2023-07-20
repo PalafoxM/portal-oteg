02a314176051d368f0fff49be4519b45594c04ed
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-07-19 23:30:33 -0600

    rutas

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False#os.getenv('DEBUG')
 # Imprime el valor de DEBUG en la consola
 print("DEBUG:", DEBUG)
 
