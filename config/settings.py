fe268d1721893ca37022f32317e81ec1f4b70de8
David Mendoza Navarrete <dmendoza064@gmail.com>
2023-07-25 16:36:53 -0600

    se elimino la libreria

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'crum.CurrentRequestUserMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
